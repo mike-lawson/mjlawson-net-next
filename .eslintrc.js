8c6bbd18e3b59fd34873d40d105ebe53bc10ffbb
Mike Lawson <mike@mjlawson.net>
2020-12-18 22:01:45 -0500

    Scaffold application
    - Configure eslint + prettier rules
    - Set up tailwind, postcss + stylelint
    - Create basic index page for blog

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    // Props spreading is fine by me for delegated props
+    'react/jsx-props-no-spreading': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+  },
+};
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <div className="py-20">
+        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">My home page</h1>
+      </div>
+    </div>
+  );
+}
